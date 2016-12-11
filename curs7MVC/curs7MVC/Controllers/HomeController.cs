8cf6238339a260075137820bf9ecef834f0a4003
anamariaciuhan <anamaria.ciuhan@gmail.com>
2016-12-12 01:04:30 +0200

    mvc

---
+++ curs7MVC/curs7MVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace curs7MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ActionName("About")]
+
+       public ActionResult SomeMethodName()
+        {
+            return View();
+        }
+        public int Calc()
+        {
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                
+                sum += i;
+            }
+            return sum;
+        }
+
+        public int DisplayCalc()
+        {
+            return Calc();
+        }
+        [HttpPost()]
+        public string VerbsTest()
+        {
+            return "Hello";
+        }
+    }
+}
---
+++ curs7MVC/curs7MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="curs7MVC.MvcApplication" Language="C#" %>
